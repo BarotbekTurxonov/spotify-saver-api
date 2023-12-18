ed2a7965bc3f048c1faede2766514625f4354fde
Barotbek Turxonov <109985351+BarotbekTurxonov@users.noreply.github.com>
2023-12-18 20:46:28 +0500

    Update spotify_search.py

--- spotify_search.py
+++ spotify_search.py
@@ -1,8 +1,10 @@
 import spotipy,json
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = '93dfe93f21fb46aaad10947eadfde562'
-client_secret = '36bb6e205ff143c6aeab7c3eb9bafd3a'
+client_id = 'your_spotify_clent_id_here'
+client_secret = 'your_spotify_client_secrect_here'
+
+# If you have any questions about client_id and client_secret, welcome to ask Telegram: @ai_junior
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
